7b0440a73f46aa4639dced67eb678a73a3debb6c
UIS-E3T <comdiguis@saber.uis.edu.co>
2019-09-20 04:08:30 -0500

    mm

---
+++ program_objetos/leccion1.py
@@ -0,0 +1,10 @@
+class b_add_ff:
+    def ___init___(self):
+        print ('me ejecuto primero')
+    def funtion(self):
+        print ('soy una funcion')
+
+misumador=b_add_ff()
+# print(misumador)
+misumador.funtion()
+print ('he terminado')    
